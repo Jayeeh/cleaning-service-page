69a891d8664553b36c8d423e094ac535ddf6f45a
Jayeeh <erroljustinecasbadillo23@gmail.com>
2024-09-19 00:00:30 +0800

    Adding Helpful Comment as Guide

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Select buttons and elements from the DOM
 const allButton = document.querySelector("#allButton");
 const insideButton = document.querySelector("#insideButton");
 const outsideButton = document.querySelector("#outsideButton");
@@ -13,33 +14,40 @@ const cleaningPhoto = document.querySelector("#cleaningPhoto");
 const cleaningEstimate = document.querySelector(".cleaningEstimate");
 const estimates = document.getElementsByTagName("li");
 
+// Function to hide the banner
 function hideBanner() {
   document.getElementById("banner").style.display = "none";
 }
 
+// Function to show outside services and hide the banner
 function showOutside() {
   hideBanner();
   outsideServices[0].classList.remove("hide");
 }
 
+// Function to show inside services and hide the banner
 function showInside() {
   hideBanner();
   insideServices[0].classList.remove("hide");
 }
 
+// Function to show both inside and outside services
 function showAll() {
   showOutside();
   showInside();
 }
 
+// Function to show the estimate banner
 function showEstimateBanner() {
   estimateBanner.classList.remove("hide");
 }
 
+// Function to show the clear estimate button
 function showClearButton() {
   clearEstimateButton.classList.remove("hide");
 }
 
+// Function to add lawn service estimate
 function addLawn() {
   showEstimateBanner();
   showClearButton();
@@ -47,6 +55,7 @@ function addLawn() {
   lawnPhoto.setAttribute("src", "https://mimo.app/i/piggy-bank.png");
 }
 
+// Function to add cleaning service estimate
 function addCleaning() {
   showEstimateBanner();
   showClearButton();
@@ -54,16 +63,23 @@ function addCleaning() {
   cleaningPhoto.setAttribute("src", "https://mimo.app/i/piggy-bank.png");
 }
 
+// Function to clear all estimates and reset photos
 function clearEstimate() {
+  // Hide the estimate banner and clear button
   estimateBanner.classList.add("hide");
   clearEstimateButton.classList.add("hide");
+
+  // Hide all estimate list items
   Array.from(estimates).forEach(el => {
     el.classList.add("hide");
   });
+
+  // Reset photos to default images
   lawnPhoto.src = "https://mimo.app/i/colorful-flowers.png";
   cleaningPhoto.src = "https://mimo.app/i/cleaning.png";
 }
 
+// Add event listeners to buttons
 allButton.addEventListener("click", showAll);
 outsideButton.addEventListener("click", showOutside);
 insideButton.addEventListener("click", showInside);
